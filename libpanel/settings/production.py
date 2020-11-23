f145bc53d8a147b9949d82a074f4936f5b2e61c6
--add <--add>
2020-11-23 15:13:18 -0300

    se cambio DB de production, enlazada con heroku

--- libpanel/settings/production.py
+++ libpanel/settings/production.py
@@ -11,10 +11,10 @@ ALLOWED_HOSTS = ['librarywithdjango.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'librarydb',
-        'USER':'postgres',
-        'PASSWORD':'balumba540...---',
-        'HOST':'127.0.0.1',
+        'NAME': 'd9hi4216lam6bb',
+        'USER':'rbmfbvoxewcjmj',
+        'PASSWORD':'9220912fb8979b5844d013ae2a24dab5e416d709425329488cae515d6fad0f7a',
+        'HOST':'ec2-23-23-88-216.compute-1.amazonaws.com',
         'DATABASE_PORT':'5432',
     }
 }
